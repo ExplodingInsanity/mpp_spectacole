64fcdc12a535d07ab6f6e83bb113b448719237ae
Mihai Ungureanu <32022907+ExplodingInsanity@users.noreply.github.com>
2021-12-29 14:56:32 +0200

    added modules

--- src/main/java/com/example/ppd_spectacole/PpdSpectacoleApplication.java
+++ src/main/java/com/example/ppd_spectacole/PpdSpectacoleApplication.java
@@ -1,13 +1,8 @@
 package com.example.ppd_spectacole;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
 public class PpdSpectacoleApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(PpdSpectacoleApplication.class, args);
     }
 
 }
